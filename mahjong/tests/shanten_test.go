415c55ce39d462cc214f30fb73d2d332e9424d3d
hphphp123321 <390441447@qq.com>
2023-03-28 00:48:57 +0800

    initial commit

--- mahjong/tests/shanten_test.go
+++ mahjong/tests/shanten_test.go
@@ -6,11 +6,8 @@ import (
 	"github.com/dnovikoff/tempai-core/hand/calc"
 	"github.com/dnovikoff/tempai-core/hand/shanten"
 	"github.com/dnovikoff/tempai-core/hand/tempai"
-	"github.com/dnovikoff/tempai-core/score"
 	"github.com/dnovikoff/tempai-core/tile"
-	"github.com/dnovikoff/tempai-core/yaku"
 	"github.com/hphphp123321/mahjong-go/mahjong"
-	"strconv"
 	"testing"
 )
 
@@ -38,50 +35,6 @@ func TestShanten(t *testing.T) {
 	for _, tileID := range tiles {
 		tenhaiSlice = append(tenhaiSlice, int(tileID))
 	}
-}
-
-func TestTenhai(t *testing.T) {
-	generator := compact.NewTileGenerator()
-	a := compact.NewInstances()
-	handTiles := tile.Tiles{5, 6, 14, 15, 17, 17, 33, 33, 4, 16}
-	hand := generator.Tiles(handTiles)
-	a.Add(hand)
-
-	declared := []calc.Meld{calc.Kan(tile.Tile(21))}
-	melds := calc.Melds{}
-	melds = append(melds, declared...)
-	cal := calc.Declared(melds)
-	results := tempai.Calculate(a, cal)
-	winTile := generator.Instance(tile.Tile(33))
-	ctx := &yaku.Context{
-		Tile:        winTile,
-		SelfWind:    0,
-		RoundWind:   0,
-		DoraTiles:   tile.Tiles{33},
-		UraTiles:    tile.Tiles{17},
-		Rules:       yaku.RulesTenhouRed(),
-		IsTsumo:     true,
-		IsRiichi:    false,
-		IsIpatsu:    false,
-		IsDaburi:    false,
-		IsLastTile:  false,
-		IsRinshan:   false,
-		IsFirstTake: false,
-		IsChankan:   false,
-	}
-
-	yakuResult := yaku.Win(results, ctx, nil)
-
-	scoreResult := score.GetScoreByResult(score.RulesTenhou(), yakuResult, 0)
-	fmt.Println(scoreResult.PayRon, scoreResult.PayTsumo, scoreResult.PayRonDealer, scoreResult.PayTsumoDealer, scoreResult.Special)
-
-	fmt.Println(yakuResult.Yakumans.String())
-	fmt.Println(yakuResult.Yaku.String())
-	fmt.Println(yakuResult.Bonuses.String())
-	fmt.Println(yakuResult.Fus.String())
-	fmt.Println(yakuResult.Sum())
-	fmt.Println(strconv.FormatBool(yakuResult.IsClosed))
-	fmt.Printf("%v\n", yakuResult.String())
 }
 
 func TestCalculate(t *testing.T) {
