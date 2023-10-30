f31aad585060b5f2fc39939144458df83652be54
hphphp123321 <390441447@qq.com>
2023-10-30 21:53:36 +0800

    add string functions to call

--- mahjong/call.go
+++ mahjong/call.go
@@ -66,7 +66,7 @@ func (call *Call) Copy() *Call {
 }
 
 func (call *Call) String() string {
-	return call.CallTiles.String()
+	return call.CallType.String() + ": " + call.CallTiles.String()
 }
 
 func (call *Call) UTF8() string {
